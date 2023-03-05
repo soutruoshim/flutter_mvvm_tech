e12d5434ba15077df0ad730b34303f43a4e7ce36
SrhDP <sout.rahim@gmail.com>
2023-03-05 09:14:29 +0700

    application class

---
+++ android/app/src/main/kotlin/com/srhdp/fluttermvvm/flutter_mvvm_tech/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.srhdp.fluttermvvm.flutter_mvvm_tech
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+
+  MyApp._internal();   // private named constructor
+
+  static final MyApp instance = MyApp._internal(); // single instance -- singleton
+
+  factory MyApp() => instance;    // factory for class instance
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
